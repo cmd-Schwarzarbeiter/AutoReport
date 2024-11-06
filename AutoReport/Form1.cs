b67053ae3b522a1ccd87afbec92d34048405d0fa
cmd-Schwarzarbeiter <ossivonstasi@web.de>
2024-11-06 15:20:58 +0100

    Changes to be committed: 	modified:   AutoReport/Form1.cs 	new file:   AutoReport/edgeAutomation.cs

--- AutoReport/Form1.cs
+++ AutoReport/Form1.cs
@@ -344,21 +344,8 @@ namespace AutoReport
             groupbox_main.Controls.Add(vScrollBar_Serv);
             //vScrollBar_Serv.Scroll += vScrollBarServ_Scroll;
 
-            /*
-            // Display the program name and steps
-            foreach (var programm in programmverzeichnis.Programme)
-            {
-                MessageBox.Show($"Programmname: {programm.Programmname}");
 
-                foreach (var schritt in programm.Schritte)
-                {
-                    MessageBox.Show($"Schrittart: {schritt.Schrittart}, Schrittdetail: {schritt.Schrittdetail}, " +
-                                    $"Text: {schritt.Text}, Waittime: {schritt.Waittime}, Schrittcheck: {schritt.Schrittcheck}");
-                }
-            }
-            */
-
-            //fuer jeden Eintrag in "Server" eine Combobox gef�llt mit dropboxen erstellen und dropboxen setzen
+            //fuer jeden Eintrag in "Server" eine Combobox gefuellt mit dropboxen erstellen und dropboxen setzen
             foreach (var server in serverCollection.Servers)
             {
                 ///////////diesen gesamten Block ich eine Funktion Ueberfuehren!!!!------------------------------------------------------------------------------------------------------------
@@ -551,62 +538,4 @@ namespace AutoReport
         [XmlElement("Schrittcheck")]
         public string Schrittcheck { get; set; }
     }
-
-    /*
-    Selenuimtestprogramm:
-
-    using static System.Net.Mime.MediaTypeNames;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using System.Threading;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Xml.Linq;
-
-//Infos: https://www.toolsqa.com/selenium-c-sharp/
-
-IWebDriver EdgeDriver = new EdgeDriver();
-
-EdgeDriver.Url = "https://de.wikipedia.org";
-EdgeDriver.Manage().Window.Maximize();
-//EdgeDriver.Manage().Timeouts().ImplicitWait(TimeSpan.FromSeconds(10));
-Thread.Sleep(1000);
-
-//try!!!!!
-EdgeDriver.FindElement(By.Name("search")).SendKeys("Bitcoin" + Keys.Enter);
-Thread.Sleep(1000);
-//EdgeDriver.FindElement(By.XPath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).Click();
-Thread.Sleep(1000);
-
-/*
-(By.Id("submit")
-(By.Name("firstname")
-(By.TagName("button"))
-(By.XPath("Element XPATHEXPRESSION"))
-
-
-
-IWebElement parentElement = driver.FindElement(By.ClassName("button"));
-	IWebElement childElement = parentElement.FindElement(By.Id("submit"));
-	childElement.Submit();
-
-
-
-IWebElement element = driver.FindElement(By.LinkText("Partial Link Test"));
-	element.Clear();
-
-	//Or can be identified as 
-	IWebElement element = driver.FindElement(By.PartialLinkText("Partial");
-	element.Clear();
-
-
-
-    Console.WriteLine("Succsess!");
-//EdgeDriver.FindElement(By.XPath"//*[@data-tab-key").Click();
-
-Thread.Sleep(10000);
-//Console.ReadLine();
-EdgeDriver.Quit();
-    */
-
 }
---
+++ AutoReport/edgeAutomation.cs
@@ -0,0 +1,54 @@
+using static System.Net.Mime.MediaTypeNames;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+//Infos: https://www.toolsqa.com/selenium-c-sharp/
+
+IWebDriver EdgeDriver = new EdgeDriver();
+
+EdgeDriver.Url = "https://de.wikipedia.org";
+EdgeDriver.Manage().Window.Maximize();
+//EdgeDriver.Manage().Timeouts().ImplicitWait(TimeSpan.FromSeconds(10));
+Thread.Sleep(1000);
+
+//try!!!!!
+EdgeDriver.FindElement(By.Name("search")).SendKeys("Bitcoin" + OpenQA.Selenium.Keys.Enter);
+//EdgeDriver.FindElement(By.Name("search")).SendKeys("Bitcoin" + Keys.Enter);
+Thread.Sleep(1000);
+//EdgeDriver.FindElement(By.XPath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).Click();
+Thread.Sleep(1000);
+
+/*
+(By.Id("submit")
+(By.Name("firstname")
+(By.TagName("button"))
+(By.XPath("Element XPATHEXPRESSION"))
+
+
+
+IWebElement parentElement = driver.FindElement(By.ClassName("button"));
+	IWebElement childElement = parentElement.FindElement(By.Id("submit"));
+	childElement.Submit();
+
+
+
+IWebElement element = driver.FindElement(By.LinkText("Partial Link Test"));
+	element.Clear();
+
+	//Or can be identified as 
+	IWebElement element = driver.FindElement(By.PartialLinkText("Partial");
+	element.Clear();
+
+
+
+    Console.WriteLine("Succsess!");
+//EdgeDriver.FindElement(By.XPath"//*[@data-tab-key").Click();
+
+Thread.Sleep(10000);
+//Console.ReadLine();
+EdgeDriver.Quit();
+    */
